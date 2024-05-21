12d3d0212e471e6223ecbb020746e2f15289694e
Lucas Fontinele <contato@fontinele.dev>
2024-05-21 02:16:20 -0300

    chore: changes

---
+++ android/app/src/main/java/com/superfrete/web/challenge/MainActivity.java
@@ -0,0 +1,5 @@
+package com.superfrete.web.challenge;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.superfrete.web.challenge',
+  appName: 'superfrete-web-challenge',
+  webDir: 'dist'
+};
+
+export default config;
